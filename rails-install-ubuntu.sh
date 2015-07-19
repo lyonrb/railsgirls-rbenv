85303ec978a6f582c64721c2d48b8eca14f34c02
monochromegane <dev.kuro.obi@gmail.com>
2015-07-19 23:51:13 +0900

    Use udzura shell.

--- rails-install-ubuntu.sh
+++ rails-install-ubuntu.sh
@@ -1,52 +1,66 @@
 #!/bin/bash
-
+ 
 set -e
-
-echo "Updates packages. Asks for your password."
+ 
+echo "パッケージを最新にします。パスワードを聞かれることがあります。"
+sudo -v
 sudo apt-get update -y
-
-echo "Installs packages. Give your password when asked."
-sudo apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev nodejs libffi-dev -y
-
-echo "Installs ImageMagick for image processing"
+ 
+echo "必要なパッケージをインストールします。"
+sudo apt-get install git build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev nodejs -y
+ 
+echo "ImageMagickを追加でインストールします"
 sudo apt-get install imagemagick --fix-missing -y
-
-echo "Installs rbenv for handling Ruby installation"
+ 
+echo "Rubyの管理のため、rbenvとruby-buildをインストールします。"
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+ 
 source ~/.bashrc
-rbenv --version
-
-echo "Installs Ruby"
-rbenv install 2.2.1
-rbenv global 2.2.1
-rbenv rehash
-
-gem install bundler --no-rdoc --no-ri
-gem install rails --no-rdoc --no-ri
-
+ 
+echo "Rubyをインストールします！"
+rbenv install 2.2.3
+rbenv global 2.2.3
+ 
+gem install bundler --no-documents
+ 
+echo "Railsをインストールします！"
+gem install rails --no-documents
+ 
 echo -e "\n- - - - - -\n"
-echo -e "Now we are going to print some information to check that everything is done:\n"
-
-echo -n "Should be sqlite 3.8.1 or higher: sqlite "
+echo -e "インストールされたバージョンが正しいか確認します:\n"
+ 
+echo -n "sqlite は 3.8.1 以上であるべきです:        "
 sqlite3 --version
-echo -n "Should be rbenv 0.4.0:                   "
-rbenv --version | sed 's/rbenv //; s/-.*$//'
-echo -n "Should be ruby 2.2.1:                    "
+ 
+echo -n "rbenv は 0.4.0 以上であるべきです:         "
+rbenv --version | awk '{print $1}'
+ 
+echo -n "ruby-build は 20150413 以降であるべきです: "
+ruby-build --version | awk '{print $1}'
+ 
+echo -n "Ruby は 2.2.1 以上であるべきです:          "
 ruby -v | cut -d " " -f 2
-echo -n "Should be Rails 4.2.1 or higher:         "
+ 
+echo -n "Rails は 4.2.1 以上であるべきです:         "
 rails -v
+ 
 echo -e "\n- - - - - -\n"
+ 
+echo "バージョンは合っていましたか？
 
-echo "If the versions match, everything is installed correctly. If the versions
-don't match or errors are shown, something went wrong with the automated process
-and we will help you do the installation the manual way at the event.
-
-Congrats!
-
-Make sure that all works well by running the application generator command:
+おめでとうごさいます！
+以下のコマンドが実行できて、無事動作するか確認してみてください:
     $ rails new railsgirls
 
-If you encounter the message:
-    The program 'rails' is currently not installed."
+もし、以下のようなメッセージが出た場合:
+    The program 'rails' is currently not installed.
+                                                                                 
+以下のコマンドを実行しなおしてください。
+    source ~/.bashrc
+
+Enjoy Programming!
+"
